5e6a843dcfdcc708793fa7ad922fa1d17d6303fc
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-04 15:30:57 -0300

    Update aula03.js

--- aula03/aula03.js
+++ aula03/aula03.js
@@ -1,6 +1,18 @@
 /*
-LET
-VAR
+LET 
+VAR 
 CONST
 */
 
+'use strict'
+function teste() {
+    if (true) {
+        let nome = 'Claudio'
+        console.log('dentro do IF do teste: ' + nome)
+    }
+    console.log('dentro do teste: ' + nome)
+}
+
+teste()
+
+console.log(nome)
