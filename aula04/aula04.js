4a34a31837336678b7c7e98b4f165611f0fd306f
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-04 18:14:13 -0300

    aula_04

---
+++ aula04/aula04.js
@@ -0,0 +1,17 @@
+/*
+Operadores aritméticos
++ soma
+- subtração
+/ divisão
+* multiplicação
+% resto da divisão
+++ ou += incremento
+-- ou -= decremento
+*/
+
+let num1 = 0, num2 = 0, res = 0
+num1 = 5
+num2 = 10
+res = num1 + num2
+console.log(res)
+console.log(num2 - num1)
