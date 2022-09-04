de2c8bd0b8d45f3d4993f6f01ed0c6ae4faa0c20
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-03 23:13:43 -0300

    aula_02

---
+++ aula02/aula02.js
@@ -0,0 +1,5 @@
+'use strict' // modo estrito ou restrito = é obrigatório declarar a variável
+let nome='Claudio'
+console.log('CFB Cursos') // string - texto
+console.log(nome) // variável
+console.log('Nome: ' + nome) // string concatenado com variável
