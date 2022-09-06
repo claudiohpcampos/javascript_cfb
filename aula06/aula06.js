a68eedc860d181ca73b6345aa298f5a91097114a
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-06 17:27:30 -0300

    aula_06

---
+++ aula06/aula06.js
@@ -0,0 +1,8 @@
+let n1 = 1
+let n2 = 2
+
+let v1 = {nome:"Claudio"}
+let v2 = {nome:"Claudio"}
+
+console.log(!(n1 != n2))
+console.log(v1 === v2) // mesmo os objetos sendo === estão em variáveis diferentes - false
