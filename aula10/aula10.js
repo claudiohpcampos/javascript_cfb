f992697315010326dcae78fcfcc30f7ae3bd5b06
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-06 20:07:11 -0300

    aula_10

---
+++ aula10/aula10.js
@@ -0,0 +1,19 @@
+//Operador Ternário ou operações Ternários
+
+let num = 10
+
+//res = (num % 2 ? 'IMPAR' : 'PAR') 
+res = (!(num % 2) ? 'PAR' : 'IMPAR')
+
+console.log(res)
+
+//0 = False
+//1 = True
+
+//Teste lógico ? se verdadeiro : se falso
+
+/*if(res==0) {
+    console.log('PAR')
+} else {
+    console.log('IMPAR')
+}*/
