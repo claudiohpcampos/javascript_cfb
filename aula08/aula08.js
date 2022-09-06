023733931d7884224a368cab67230fc2c481385a
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-06 19:04:52 -0300

    aula_08

---
+++ aula08/aula08.js
@@ -0,0 +1,14 @@
+/*
+Operadores Bitwise - faz a operação bit a bit do elemento
+& - retorna 0 -> equivalente ao AND
+| - retorna 1 -> eqquivalente ao OR
+^ - onde tem equivalência retorna 0 (sendo 0 ou 1), onde não tem retorna 1
+>> - desloca a quantidade de bit para a direita
+<< - desloca a quantidade de bit para a esquerda
+*/
+
+let n1 = 30
+
+res = n1 >> 1
+
+console.log(res)
