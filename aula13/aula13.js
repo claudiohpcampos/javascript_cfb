22de9fdec5fe219947c96755f30a623ccc033e86
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-06 21:47:10 -0300

    aula_13

---
+++ aula13/aula13.js
@@ -0,0 +1,11 @@
+// Comando Condicional IF e IF ELSE
+
+let energia = 100
+let clima = "chuva"
+
+if(energia > 70 && clima == "sol") {
+    console.log("Vou à praia")
+}else {
+    console.log("Vou ao cinema")
+}
+console.log("Fim do Programa")
