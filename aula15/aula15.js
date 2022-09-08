12bca0a7631cfe29e42e43a20e829634a548f100
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-09-08 19:24:42 -0300

    aula_15

---
+++ aula15/aula15.js
@@ -0,0 +1,12 @@
+// Loop FOR - Repetição (cada repetição é chamada de iteração)
+// loop defindos - qdo soubermos a quantidades(FOR) , indefinidos - qdo não soubermos(WHILE e DO WHILE)
+
+console.log('Início do Programa')
+for(let i = 0; i < 20; i++) {
+    if(i % 2 == 0) {
+        console.log(i + ' é par')
+    } else {
+        console.log(i + ' é impar')
+    }
+}
+console.log('Fim do Programa')
